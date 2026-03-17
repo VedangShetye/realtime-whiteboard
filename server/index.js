f74cfbbcb2da8f78506fa0abfd7ccfbd45001fec
Vedang Shetye <vedangshetye@gmail.com>
2026-03-17 12:58:14 +0530

    feat: setup Node + Socket.io server with CORS

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id)
+
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id)
+  })
+})
+
+const PORT = 3001
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
